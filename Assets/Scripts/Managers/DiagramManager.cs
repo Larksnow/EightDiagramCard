a5394c44efe70615dfb747f4c396a0954a25fb4d
Alan Fan <fwt2584190223@gmail.com>
2024-11-25 23:57:10 -0500

    update diagram manager logic to correclt apply card effect

--- Assets/Scripts/Managers/DiagramManager.cs
+++ Assets/Scripts/Managers/DiagramManager.cs
@@ -33,11 +33,28 @@ public class DiagramManager : MonoBehaviour
     }
     #endregion
 
-    public void ApplyDiagramEffect(DiagramDataSO diagramData)
+    public void ApplyDiagramEffect(DiagramDataSO diagramData, CardDataSO upYao, CardDataSO midYao, CardDataSO downYao)
     {
-        //TODO: ADD a select targetCharacter function using switch case
+        // TODO：首先触发所有组成此卦的爻的Effect， up, mid, down 顺序
+        foreach (var effect in upYao.effects)
+        {
+            // 爻的效果要么直接作用在玩家身上，要么把自己的effect直接给下面的卦
+            // effect.Execute();
+        }
+        foreach (var effect in midYao.effects)
+        {
+            // 爻的效果要么直接作用在玩家身上，要么把自己的effect直接给下面的卦
+            // effect.Execute();
+        }
+        foreach (var effect in downYao.effects)
+        {
+            // 爻的效果要么直接作用在玩家身上，要么把自己的effect直接给下面的卦
+            // effect.Execute();
+        }
+        //Pending: ADD a select targetCharacter function using switch case
         foreach (var effect in diagramData.effects)
         {
+            //思考一下在哪里，由谁去选取正确的对象，可以由本脚本通过switch cases来选取（所有八卦的作用对象，甚至还要写随机选取），也可以由外部传入一个target
             effect.Execute(player);
         }
     }
