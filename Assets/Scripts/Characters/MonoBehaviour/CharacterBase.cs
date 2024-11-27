e8e2ed922f04a2b6d5737ad061e488a7b10a1a85
Alan Fan <fwt2584190223@gmail.com>
2024-11-27 01:06:53 -0500

    Update a Miti Buff effect

--- Assets/Scripts/Characters/MonoBehaviour/CharacterBase.cs
+++ Assets/Scripts/Characters/MonoBehaviour/CharacterBase.cs
@@ -6,19 +6,30 @@ public class CharacterBase : MonoBehaviour
 {
     public int maxHp;
     public IntVariable hp;
+    public IntVariable mitiNumber;
     // Property to manage the character's current health and trigger events when modified
     // These are stored in a SO called CharacterHP (IntVariable type)
     public int CurrentHP {get => hp.currentValue; set => hp.SetValue(value);}
     public int MaxHP {get => hp.maxValue;}
+    public int CurrentMiti {get => mitiNumber.currentValue; set => mitiNumber.SetValue(value);}
     
+    // This list store all the buff SOs which stores the number
+    public List<IntVariable> buffList;
     public bool isDead;
     protected virtual void Start(){
         hp.maxValue = maxHp;
         CurrentHP = MaxHP;
+        //TODO: Load all buff SO using addressable asset (virtual, player and enemy have their own SOs)
     }
 
+    // 所有伤害向上取整，所有受伤向下取整
     public virtual void TakeDamage(int damage)
     {
+        // TODO:: Apply miti before taking damage -> （damage * 0.75）向下取整 
+        if (CurrentMiti > 1) {
+            damage = Mathf.FloorToInt(0.75f * damage);
+            UpdateMitiNumber(-1);
+        }
         if (CurrentHP > damage)
         {
             CurrentHP -= damage;
@@ -29,5 +40,19 @@ public class CharacterBase : MonoBehaviour
             //TODO: Die
             isDead = true;
         }
-    } 
+    }
+
+    public virtual void Die(){}
+
+    public virtual void Heal(int healAmount){}
+
+    public virtual void UpdateMitiNumber(int value) // decrease one miti
+    {
+        Debug.Log($"Before Update: CurrentMiti = {CurrentMiti}");
+        CurrentMiti += value;
+        Debug.Log($"After Update: CurrentMiti = {CurrentMiti}");
+        if (CurrentMiti < 0) {
+            CurrentMiti = 0;
+        }
+    }
 }
---
+++ Assets/Scripts/Effects/Buff/Miti Effect.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MitiEffect", menuName = "Buff/Miti Effect")]
+public class MitiEffect : Effect
+{
+    // 坚硬（受到伤害降低25%，受击后减少一层）
+    int number = 1; //每次该效果施加的层数
+    public override void Execute(CharacterBase target, DiagramDataSO diagramData)
+    {
+        Debug.Log("MitiEffect");
+        switch (targetType)
+        {
+            case EffectTargetType.Self:
+                Debug.Log($"Passing {number} to {target}");
+                target.UpdateMitiNumber(number);
+                break;
+            case EffectTargetType.Single:
+            case EffectTargetType.All:
+                break;
+        }
+    }
+
+}
