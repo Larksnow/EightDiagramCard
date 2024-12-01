5ac8ebbbfbea7de8929fda6c9c3787e9c2a7adf6
Alan Fan <fwt2584190223@gmail.com>
2024-12-01 03:23:54 -0500

    Add day night switch

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource day;
+    public AudioSource night;
+    public void SwitchBGM()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (Indicator.main.isDay)
+        {
+            night.DOFade(0, 5f).SetEase(Ease.InOutSine);
+            day.DOFade(1, 5f).SetEase(Ease.InOutSine);
+        }else
+        {
+            night.DOFade(1, 0.8f).SetEase(Ease.InOutSine);
+            day.DOFade(0, 5f).SetEase(Ease.InOutSine);
+        }
     }
 }
---
+++ Assets/Scripts/UI/BackGroundChange.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BackGroundChange : MonoBehaviour
+{
+    public SpriteRenderer day;
+    public SpriteRenderer night;
+    void Awake()
+    {
+
+    }
+    [ContextMenu("Test")]
+    public void SwitchBackround()
+    {
+        if (Indicator.main.isDay)
+        {
+            day.DOFade(1, 0.8f);
+        }else
+        {
+            day.DOFade(0, 0.8f);
+        }
+    }
+}
