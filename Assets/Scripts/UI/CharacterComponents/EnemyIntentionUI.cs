0d27bf95f26dfe64527373ef7245a3945efb8757
Rimor <2590548416@qq.com>
2024-12-09 07:59:40 +0800

    Add Enemy Intention UI fade in / out effect.

--- Assets/Scripts/UI/CharacterComponents/EnemyIntentionUI.cs
+++ Assets/Scripts/UI/CharacterComponents/EnemyIntentionUI.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -8,43 +10,70 @@ public class EnemyIntentionUI : MonoBehaviour
     public GameObject effectIconPrefab;
     public Vector3 centerPosition = new(0, 0, 0);
     public float spacing = 0.2f;
+
+    private FadeInOutHandler fadeInOutHandler;
     private List<EnemyEffect> nextTurnEffects = new();
+    private float iconWidth;
+
+    private void Awake()
+    {
+        iconWidth = effectIconPrefab.GetComponent<SpriteRenderer>().bounds.size.x;
+        fadeInOutHandler = GetComponent<FadeInOutHandler>();
+    }
 
     #region Event Listening
 
     public void ShowUI()
     {
-        // TODO: 淡入淡出效果
+        StartCoroutine(ShowUICoroutine());
+    }
+
+    #endregion
+
+    private IEnumerator ShowUICoroutine()
+    {
+        yield return ClearUICoroutine();
+
         nextTurnEffects = currentEnemy.GetNextTurnEffects();
-
-        ClearUI();
-
         var numEffects = nextTurnEffects.Count;
-        if (numEffects == 0) return;
+        if (numEffects == 0) yield break;
 
         // 设置左右对称的分布
         for (var i = 0; i < numEffects; i++)
         {
-            var iconWidth = effectIconPrefab.GetComponent<SpriteRenderer>().bounds.size.x;
             // 计算当前效果的位置
             var offset = (i - (numEffects - 1) / 2f) * (iconWidth + spacing);
             var iconPosition = centerPosition + new Vector3(offset, 0, 0);
+            var nextEffect = nextTurnEffects[i];
 
             // 创建图标
             var icon = Instantiate(effectIconPrefab, iconPosition, Quaternion.identity);
-            icon.GetComponent<SpriteRenderer>().sprite = nextTurnEffects[i].icon;
-            icon.GetComponentInChildren<TextMeshPro>().text = nextTurnEffects[i].GetRuntimeValue().ToString();
+            icon.GetComponent<SpriteRenderer>().sprite = nextEffect.icon;
+            var text = icon.GetComponentInChildren<TextMeshPro>();
+            if (nextEffect.effectType is EnemyEffectType.Damage or EnemyEffectType.Shield)
+            {
+                text.text = nextEffect.GetRuntimeValue().ToString();
+            }
+            else
+            {
+                // 非数值型意图(除了攻击、防御)不显示数值
+                text.enabled = false;
+            }
+
             icon.transform.SetParent(transform, false);
         }
+        fadeInOutHandler.FadeIn();
     }
 
-    private void ClearUI()
+    private IEnumerator ClearUICoroutine()
     {
-        foreach (Transform child in transform)
+        fadeInOutHandler.FadeOut(() =>
         {
-            Destroy(child.gameObject);
-        }
+            foreach (Transform child in transform)
+            {
+                Destroy(child.gameObject);
+            }
+        });
+        yield return new WaitForSeconds(fadeInOutHandler.fadeDuration * 1.2f);
     }
-
-    #endregion
 }
