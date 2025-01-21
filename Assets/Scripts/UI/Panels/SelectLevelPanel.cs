5ed38e2b0f649e2d98800c554ba090c6810fe699
Rimor <2590548416@qq.com>
2025-01-21 22:04:11 +0800

    Add lost event variables binding to Menu buttons and SelectLevelPanel buttons.

--- Assets/Scripts/UI/Panels/SelectLevelPanel.cs
+++ Assets/Scripts/UI/Panels/SelectLevelPanel.cs
@@ -2,18 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 
-public class LevelSelectUI : FadablePanel
+public class SelectLevelPanel : FadablePanel
 {
-    
     // Hardcode for now, option1  is card option2 is blessing
     public GameObject option1;
     public GameObject option2;
     public GameAwardSO gameAwardData;
-    public GameSceneSO sceneToLoad;
 
-    [Header("Broadcast events")]
-    public ObjectEventSO LoadNextLevelEvent;
+    [Header("Broadcast events")] public ObjectEventSO loadNextLevelEvent;
 
     public override void FadeOutAfterClick(GameObject selected)
     {
@@ -21,12 +19,13 @@ public class LevelSelectUI : FadablePanel
         if (selected == option1)
         {
             gameAwardData.awardType = AwardType.Card;
-            LoadNextLevelEvent.RaiseEvent(null, this);
-        }else if (selected == option2)
+            loadNextLevelEvent.RaiseEvent(null, this);
+        }
+        else if (selected == option2)
         {
             //TODO: Change this into blessing after we made blessing
             gameAwardData.awardType = AwardType.Card;
-            LoadNextLevelEvent.RaiseEvent(null, this);
+            loadNextLevelEvent.RaiseEvent(null, this);
         }
     }
 }
